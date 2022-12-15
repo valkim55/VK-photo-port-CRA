caae0bc46af373e78561d7764c1f93f8443dc77c
valkim55 <vesper5521@gmail.com>
2022-12-14 21:21:28 -0500

    tests for PhotoList and Gallery

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Gallery from '..';
+import Gallery from '../';
 
 // since the Gallery component needs a prop of currentCategory, you can mock it
 const portrait = { name: 'portraits', description: 'Portraits of people in my life'};
 
 afterEach(cleanup)
+
 describe('Gallery is rendering', () => {
     it('renders', () => {
         render(<Gallery currentCategory={portrait} />);
     })
+});
+
+it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+});
+
+it('renders', () => {
+    const { getByTestId } = render(<Gallery currentCategory={portrait} />);
+    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
